f2a1cfbc0c741ffc55499e26459801dadfc5eb69
conkobar <connorkleine@gmail.com>
2023-09-06 21:22:09 -0500

    fixed missing install

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+  // Adjust the entry point as needed
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
@@ -10,17 +11,22 @@ module.exports = {
   module: {
     rules: [
       {
+        // This rule will match .css files
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'image-webpack-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'image-webpack-loader',
-        enforce: 'pre',
+        // This rule will match image files
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // Output path for images
+              outputPath: 'images/',
+            },
+          },
+        ],
       },
     ],
   },
