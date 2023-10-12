0630383ba8225e0bb49c75ff9570020a027c606e
conkobar <connorkleine@gmail.com>
2023-10-12 11:47:39 -0500

    task 15

--- react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -4,12 +4,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App/App';
-import { appReducer } from './reducers/uiReducer';
+import rootReducer from './reducers/rootReducer';
 
 const composeEnhancers =
   window.window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
