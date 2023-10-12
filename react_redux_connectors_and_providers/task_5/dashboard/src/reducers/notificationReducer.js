3b1d7395d4c9086d98b5195d8aefa72264085438
conkobar <connorkleine@gmail.com>
2023-10-12 14:38:20 -0500

    task 18

--- react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -3,12 +3,14 @@ import {
   FETCH_NOTIFICATIONS_SUCCESS,
   MARK_AS_READ,
   SET_TYPE_FILTER,
+  SET_LOADING_STATE,
 } from '../actions/notificationActionTypes';
 import { notificationsNormalizer } from '../schema/notifications';
 
 const defaultState = Map({
   filter: 'DEFAULT',
   notifications: Map(),
+  loading: false,
 });
 
 export const notificationsReducer = (state = defaultState, action) => {
@@ -24,11 +26,16 @@ export const notificationsReducer = (state = defaultState, action) => {
       return state.set('filter', action.payload);
     case FETCH_NOTIFICATIONS_SUCCESS:
       const normalizedNotifications = notificationsNormalizer(action.payload);
-      return state.mergeIn(['notifications'], normalizedNotifications);
+      return state.mergeDeep({
+        notifications: normalizedNotifications,
+        loading: false,
+      });
     case MARK_AS_READ:
       return state.setIn(['notifications', action.index, 'isRead'], true);
     case SET_TYPE_FILTER:
       return state.set('filter', action.filter);
+    case SET_LOADING_STATE:
+      return state.set('loading', action.payload);
     default:
       return state;
   }
